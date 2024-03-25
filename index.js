2d09e30123f306fbd36e516a4f8f4bb99e0edb27
EvgeniyaDanilovich <evgeniyadanilovich@gmail.com>
2024-03-25 20:53:13 +0300

    feat: add websocket

--- index.js
+++ index.js
@@ -1,14 +1,65 @@
 const fs = require('fs');
 const jsonServer = require('json-server');
 const path = require('path');
+const WebSocket = require('ws');
 
 const server = jsonServer.create();
+const wss = new WebSocket.Server({ server });
 
-// const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
+const clients = [];
+
+wss.on('connection', (ws) => {
+    console.log('WebSocket connected');
+
+    ws.on('message', (message) => {
+        try {
+            message = JSON.parse(message);
+            console.log(`Received message: ${message}`);
+            if (message.method) {
+                switch (message.method) {
+                    case 'connection':
+                        connectionHandler(ws, message);
+                        break;
+                    case 'addComment':
+                        broadcastConnection(ws, message);
+                        break;
+                    default:
+                        console.error('Unknown method:', message.method);
+                }
+            } else {
+                console.error('Method not provided in message:', message);
+            }
+        } catch (error) {
+            console.error('Error parsing JSON:', error);
+        }
+    });
+
+    ws.on('close', (code, reason) => {
+        console.log('WebSocket disconnected');
+        console.log(`WebSocket disconnected with code ${code}: ${reason}`);
+
+        const index = clients.indexOf(ws);
+        if (index !== -1) {
+            clients.splice(index, 1);
+        }
+    });
+
+    ws.on('error', (error) => {
+        console.error('WebSocket error:', error);
+    });
+});
+
 const router = jsonServer.router('./db.json');
+
 
 server.use(jsonServer.defaults({}));
 server.use(jsonServer.bodyParser);
+
+server.use('/socket', (req, res, next) => {
+    wss.handleUpgrade(req, req.socket, Buffer.alloc(0), (ws) => {
+        wss.emit('connection', ws, req);
+    });
+});
 
 // Эндпоинт для логина
 server.post('/login', (req, res) => {
@@ -20,10 +71,6 @@ server.post('/login', (req, res) => {
         const userFromBd = users.find(
             (user) => user.username === username && user.password === password,
         );
-
-        // if (userFromBd) {
-        //     return res.json(userFromBd);
-        // }
 
         if (userFromBd) {
             if (userFromBd.status === 'Active') {
@@ -39,19 +86,27 @@ server.post('/login', (req, res) => {
     }
 });
 
-// проверяем, авторизован ли пользователь
-// eslint-disable-next-line
-server.use((req, res, next) => {
-    // if (!req.headers.authorization) {
-    //     return res.status(403).json({ message: 'AUTH ERROR' });
-    // }
-
-    next();
-});
-
 server.use(router);
 
 // запуск сервера
 server.listen(8000, () => {
     console.log('server is running on 8000 port');
 });
+
+const connectionHandler = (ws, msg) => {
+    clients.push(ws);
+    ws.id = msg.id;
+    broadcastConnection(ws, msg);
+};
+
+const broadcastConnection = (ws, msg) => {
+    clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            try {
+                client.send(JSON.stringify(msg));
+            } catch (error) {
+                console.error('Error broadcasting message:', error);
+            }
+        }
+    });
+};
